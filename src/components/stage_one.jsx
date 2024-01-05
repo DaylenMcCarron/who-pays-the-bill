4094db6437152ef1fe27bd688247f13db36e614d
DaylenMcCarron <100991420+DaylenMcCarron@users.noreply.github.com>
2024-01-05 18:00:39 +0000

    add: create stage one form

--- src/components/stage_one.jsx
+++ src/components/stage_one.jsx
@@ -1,11 +1,41 @@
-
-
+import { Button, Form, Alert } from "react-bootstrap";
+import { useRef } from "react";
 
 const Stage1 = () => {
 
+    const textInput = useRef();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const value = textInput.current.value;
+
+        // Run Validation
+        // add to list
+        
+
+        console.log(value);
+        textInput.current.value = '';
+    }
+
     return(
         <>
-            Stage 1
+            <Form onSubmit={handleSubmit} className="mt-4">
+                <Form.Group>
+                    <Form.Control
+                        type="text"
+                        placeholder="Add player name"
+                        name="player"
+                        ref={textInput}
+                    />
+                </Form.Group>
+
+                    {/* {Show Errors} */}
+
+                <Button className="miami" variant="primary" type="submit">
+                    Add player
+                </Button>
+
+            </Form>
         </>
     )
 }
